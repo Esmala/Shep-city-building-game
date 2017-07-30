33ab903b9c6ee036e41f2411368473caa32e408e
Elias Ojala <theel0ja@users.noreply.github.com>
2017-07-30 21:17:28 +0300

    Polished version.

--- functions.js
+++ functions.js
@@ -1,10 +1,27 @@
+// Function to draw object
+function drawObject(ctx, left, top, size1, size2, fillcolour) {
+  ctx.beginPath();
+  ctx.rect(left, top, size1, size2);
+  ctx.fillStyle = fillcolour;
+  ctx.fill();
+}
+
+// Function to update view
+function updateView(ctx, jsonData) {
+  var gameData = JSON.parse(jsonData, (key, objectData) => {
+    // key = Object1, etc.
+    document.getElementById("myCanvas").innerHTML = drawObject(ctx, objectData.Left, objectData.Top, objectData.Width, objectData.Height, objectData.Color);
+    return objectData;     // return the unchanged property value.
+  });
+}
+
 // Update view using JSON
-function updateWithJson(url) {
+function updateWithJson(ctx, url) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       // Update view with current json
-      updateView(this.responseText);
+      updateView(ctx, this.responseText);
     }
   };
   xhttp.open("GET", url, true);
